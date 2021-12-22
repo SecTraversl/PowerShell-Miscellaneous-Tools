0000206f0cfd4b0d59f1b4368ae013398e7d57b9
Travis Logue <SecTraversl@ProtonMail.com>
2021-12-22 12:28:53 -0800

    Last write time 2021-12-22

---
+++ Lock-Computer.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+  The "Lock-Computer" functions locks the computer by leveraging "rundll32.exe user32.dll,LockWorkStation".
+
+.EXAMPLE
+  PS C:\> Lock-Computer
+
+  
+  This locks the computer.
+
+.NOTES
+  Name: Lock-Computer
+  Author: Travis Logue
+  Version History: 1.1 | 2021-01-03 | Initial Version
+  Dependencies:
+  Notes:
+  - This was helpful in determining the approach below: https://docs.microsoft.com/en-us/powershell/scripting/samples/changing-computer-state?view=powershell-7.1
+
+  
+  .
+#>
+function Lock-Computer {
+  [CmdletBinding()]
+  param ()
+  
+  begin {}
+  
+  process {
+    rundll32.exe user32.dll,LockWorkStation
+  }
+  
+  end {}
+}
